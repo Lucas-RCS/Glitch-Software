5c74a31290500e72f8e1a5af58ee581046bee06a
Lucas-RCS <lucasribeiro2216@gmail.com>
2023-03-04 09:58:53 -0300

    a

--- public/js/app.js
+++ public/js/app.js
@@ -1,100 +1,98 @@
 // Scroll suave para link interno
 $('nav a').click(function (e) {
-	e.preventDefault();
-	var id = $(this).attr('href'),
-		menuHeight = $('nav').innerHeight(),
-		targetOffset = $(id).offset().top;
-	$('html, body').animate({
-		scrollTop: targetOffset - menuHeight
-	}, 500);
+  e.preventDefault();
+  var id = $(this).attr('href'),
+    menuHeight = $('nav').innerHeight(),
+    targetOffset = $(id).offset().top;
+  $('html, body').animate({
+    scrollTop: targetOffset - menuHeight
+  }, 500);
 });
 
 // Debounce do Lodash
 debounce = function (func, wait, immediate) {
-	var timeout;
-	return function () {
-		var context = this, args = arguments;
-		var later = function () {
-			timeout = null;
-			if (!immediate) func.apply(context, args);
-		};
-		var callNow = immediate && !timeout;
-		clearTimeout(timeout);
-		timeout = setTimeout(later, wait);
-		if (callNow) func.apply(context, args);
-	};
+  var timeout;
+  return function () {
+    var context = this, args = arguments;
+    var later = function () {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    var callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
 };
 
 // Animação ao Scroll
 (function () {
-	var $target = $('.anime'),
-		animationClass = 'anime-start',
-		offset = $(window).height() * 3 / 4;
+  var $target = $('.anime'),
+    animationClass = 'anime-start',
+    offset = $(window).height() * 3 / 4;
 
-	function animeScroll() {
-		var documentTop = $(document).scrollTop();
+  function animeScroll() {
+    var documentTop = $(document).scrollTop();
 
-		$target.each(function () {
-			var itemTop = $(this).offset().top;
-			if (documentTop > itemTop - offset) {
-				$(this).addClass(animationClass);
-			} else {
-				$(this).removeClass(animationClass);
-			}
-		});
-	}
+    $target.each(function () {
+      var itemTop = $(this).offset().top;
+      if (documentTop > itemTop - offset) {
+        $(this).addClass(animationClass);
+      } else {
+        $(this).removeClass(animationClass);
+      }
+    });
+  }
 
-	animeScroll();
+  animeScroll();
 
-	$(document).scroll(debounce(function () {
-		animeScroll();
-	}, 200));
+  $(document).scroll(debounce(function () {
+    animeScroll();
+  }, 200));
 })();
 
 
 // NAVBAR MOBILE
 class MobileNavbar {
-    constructor(mobileMenu, navList, navLinks) {
-      this.mobileMenu = document.querySelector(mobileMenu);
-      this.navList = document.querySelector(navList);
-      this.navLinks = document.querySelectorAll(navLinks);
-      this.activeClass = "active";
-  
-      this.handleClick = this.handleClick.bind(this);
-    }
-  
-    animateLinks() {
-      this.navLinks.forEach((link, index) => {
-        link.style.animation
-          ? (link.style.animation = "")
-          : (link.style.animation = `navLinkFade 0.5s ease forwards ${
-              index / 7 + 0.3
-            }s`);
-      });
-    }
-  
-    handleClick() {
-      this.navList.classList.toggle(this.activeClass);
-      this.mobileMenu.classList.toggle(this.activeClass);
-      this.animateLinks();
-    }
-  
-    addClickEvent() {
-      this.mobileMenu.addEventListener("click", this.handleClick);
-    }
-  
-    init() {
-      if (this.mobileMenu) {
-        this.addClickEvent();
-      }
-      return this;
-    }
+  constructor(mobileMenu, navList, navLinks) {
+    this.mobileMenu = document.querySelector(mobileMenu);
+    this.navList = document.querySelector(navList);
+    this.navLinks = document.querySelectorAll(navLinks);
+    this.activeClass = "active";
+
+    this.handleClick = this.handleClick.bind(this);
   }
-  
-  const mobileNavbar = new MobileNavbar(
-    ".mobile-menu",
-    ".nav-list",
-    ".nav-list li",
-  );
-  mobileNavbar.init();
-  
+
+  animateLinks() {
+    this.navLinks.forEach((link, index) => {
+      link.style.animation
+        ? (link.style.animation = "")
+        : (link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3
+          }s`);
+    });
+  }
+
+  handleClick() {
+    this.navList.classList.toggle(this.activeClass);
+    this.mobileMenu.classList.toggle(this.activeClass);
+    this.animateLinks();
+  }
+
+  addClickEvent() {
+    this.mobileMenu.addEventListener("click", this.handleClick);
+  }
+
+  init() {
+    if (this.mobileMenu) {
+      this.addClickEvent();
+    }
+    return this;
+  }
+}
+
+const mobileNavbar = new MobileNavbar(
+  ".mobile-menu",
+  ".nav-list",
+  ".nav-list li",
+);
+mobileNavbar.init();
